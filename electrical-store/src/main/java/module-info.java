58ee401290468e723c5a6d2bf48f5d1392af493c
Denis Lisenkov <lisenkov.d.y@gmail.com>
2023-02-22 11:23:44 +0300

    Fixed some moments

--- electrical-store/src/main/java/module-info.java
+++ electrical-store/src/main/java/module-info.java
@@ -1,11 +1,14 @@
-module controller {
+module store {
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+
     requires java.sql;
     requires mysql.connector.java;
 
     opens org.example.controller to javafx.fxml;
     opens org.example.model to javafx.base;
+
     exports org.example.controller;
-    exports org.example.model;
 }
